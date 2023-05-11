26843ad4d3ee661eb4d2b690fd4680f4c6850d41
jkjoy99 <jkjoy99@gmail.com>
2023-05-11 20:32:27 +0600

    first commit

---
+++ __steps__.js
@@ -0,0 +1,10 @@
+/**
+ * 1. create a post api on the server side
+ * 2. client side send data via post
+ * 3. set fetch method inside the fetch option (second parameter)
+ * 4. options will have three things: method
+ * 5. option will have header: 'content-type':'application/json' 
+ * 6. dont foreget to send data by JSON.Stringify in the body
+ * 7. On the server side: don't forget to use express.jsson() middleware
+ * 
+ * **/ 
